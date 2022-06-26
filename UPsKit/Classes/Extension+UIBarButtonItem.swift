0ea4af09398f128d90eaea65ca0008e69c435dc8
Up-s <dldbdjq@gmail.com>
2022-06-26 18:09:18 +0900

    [0.2.8] Extension+UIBarButtonItem

---
+++ UPsKit/Classes/Extension+UIBarButtonItem.swift
@@ -0,0 +1,15 @@
+//
+//  Extension+UIBarButtonItem.swift
+//  UPsKit
+//
+//  Created by YouUp Lee on 2022/06/26.
+//
+
+import UIKit
+
+extension UIBarButtonItem {
+  
+  convenience init(title: String, style: UIBarButtonItem.Style = .plain) {
+    self.init(title: title, style: style, target: nil, action: nil)
+  }
+}
