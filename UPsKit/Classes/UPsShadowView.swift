fbfc6a2dcb291ee7d1258ebccbb896cadfc94ed3
Up-s <dldbdjq@gmail.com>
2022-07-01 00:39:27 +0900

    ADD ShadowView

---
+++ UPsKit/Classes/UPsShadowView.swift
@@ -0,0 +1,41 @@
+//
+//  UPsShadowView.swift
+//  UPsKit
+//
+//  Created by YouUp Lee on 2022/07/01.
+//
+
+import UIKit
+
+final class ShadowView: UIView {
+  
+  // MARK: - Property
+  
+  private var isShadow: Bool = false
+  
+  
+  
+  // MARK: - Override
+  
+  override var bounds: CGRect {
+    didSet {
+      let shadowPath = UIBezierPath(rect: self.bounds)
+      self.layer.shadowPath = shadowPath.cgPath
+      self.setShadow()
+    }
+  }
+  
+  
+  
+  // MARK: - UI
+  
+  private func setShadow() {
+    if self.isShadow { return }
+    self.layer.shadowRadius = 5.0
+    self.layer.shadowOpacity = 0.3
+    self.layer.shadowOffset = .zero
+    self.layer.shadowColor = UIColor.darkGray.cgColor
+    self.layer.masksToBounds = false
+    self.isShadow = true
+  }
+}
