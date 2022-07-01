a3b1355fbe9585cd3636d115dcb6840750fdc82c
Up-s <dldbdjq@gmail.com>
2022-07-01 16:52:09 +0900

    UPDATE Date formatter 추가

--- UPsKit/Classes/Extension+Date.swift
+++ UPsKit/Classes/Extension+Date.swift
@@ -10,13 +10,16 @@ import Foundation
 extension Date {
   
   public enum Formatter: String {
+    
     case HHmmss = "HH:mm:ss"
+    case MdEEEEEHHmmss = "M월 d일 (EEEEE) HH:mm:ss"
     case yyyyMMdd = "yyyy-MM-dd"
   }
   
   public func toString(_ formatter: Formatter) -> String {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = formatter.rawValue
+    dateFormatter.locale = Locale(identifier: "ko_KR")
     return dateFormatter.string(from: self as Date)
   }
 }
