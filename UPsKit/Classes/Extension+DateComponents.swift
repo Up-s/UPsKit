2b91a0b054d640f1d535e622bc9c066da0f4294f
Up-s <dldbdjq@gmail.com>
2022-07-03 16:38:22 +0900

    [0.6.0] DebugLog, Extension+DateComponents 추가

---
+++ UPsKit/Classes/Extension+DateComponents.swift
@@ -0,0 +1,27 @@
+//
+//  Extension+DateComponents.swift
+//  UPsKit
+//
+//  Created by YouUp Lee on 2022/07/03.
+//
+
+import Foundation
+
+extension DateComponents {
+  
+  var getHour: String {
+    String(format: "%02d", self.hour!)
+  }
+  
+  var getMin: String {
+    String(format: "%02d", self.minute!)
+  }
+  
+  var getSec: String {
+    String(format: "%02d", self.second!)
+  }
+  
+  var getNano: String {
+    String(format: "%02d", (self.nanosecond! / 10000000))
+  }
+}
