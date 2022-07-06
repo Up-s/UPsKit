6a3814627b58cd092b88682fe0ee4e6b9ccc42c8
Up-s <dldbdjq@gmail.com>
2022-07-06 23:59:57 +0900

    [0.7.0] UPsStackView

---
+++ UPsKit/Classes/UPsStackView.swift
@@ -0,0 +1,38 @@
+//
+//  UPsStackView.swift
+//  UPsKit
+//
+//  Created by YouUp Lee on 2022/07/06.
+//
+
+public class UPsStackView: UIStackView {
+  
+  public init(axis: NSLayoutConstraint.Axis, alignment: UIStackView.Alignment, distribution: UIStackView.Distribution, spacing: CGFloat = 0.0) {
+    super.init(frame: .zero)
+    
+    self.axis = axis
+    self.alignment = .fill
+    self.distribution = .fill
+    self.spacing = spacing
+  }
+  
+  public init(axis: NSLayoutConstraint.Axis, alignment: UIStackView.Alignment? = nil, distribution: UIStackView.Distribution? = nil, spacing: CGFloat = 0.0) {
+    super.init(frame: .zero)
+    
+    self.axis = axis
+    
+    if let temp = alignment {
+      self.alignment = temp
+    }
+    
+    if let temp = distribution {
+      self.distribution = temp
+    }
+    
+    self.spacing = spacing
+  }
+  
+  required init(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
