c96eb1554b28a71ff396a7f41a8b791d0d289bd0
Up-s <dldbdjq@gmail.com>
2022-08-28 23:09:00 +0900

    UPDATE Alert

---
+++ UPsKit/Classes/UPsAlertAction.swift
@@ -0,0 +1,21 @@
+//
+//  UPsAlertAction.swift
+//  UPsKit
+//
+//  Created by YouUp Lee on 2022/07/02.
+//
+
+import UIKit
+
+public struct UPsAlertAction {
+  
+  public let title: String
+  public let style: UIAlertAction.Style
+  public let handler: ((UIAlertAction) -> Void)?
+  
+  public init(title: String, style: UIAlertAction.Style = .default, handler: ((UIAlertAction) -> Void)? = nil) {
+    self.title = title
+    self.style = style
+    self.handler = handler
+  }
+}
