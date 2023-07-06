c96fef2410dc60358ea15bab31feb78b7c761684
Up-s <dldbdjq@gmail.com>
2023-07-06 19:14:01 +0900

    UPDATE NSMutableAttributedString 추가

--- UPsKit/Classes/Extension+NSMutableAttributedString.swift
+++ UPsKit/Classes/Extension+NSMutableAttributedString.swift
@@ -10,38 +10,96 @@ import UIKit
 public extension NSMutableAttributedString {
   
   class func config(
-      text: String,
-      keyword: String,
-      font: UIFont? = nil,
-      lineOffset: CGFloat? = nil,
-      color: UIColor? = nil
+    text: String,
+    keyword: String,
+    font: UIFont? = nil,
+    color: UIColor? = nil,
+    lineSpace: CGFloat? = nil,
+    lineOffset: CGFloat? = nil
   ) -> NSMutableAttributedString {
-      let attributedString = NSMutableAttributedString(string: text)
-      
+    let attributedString = NSMutableAttributedString(string: text)
+    
+    if let font = font {
+      attributedString.addAttribute(
+        NSAttributedString.Key.font,
+        value: font,
+        range: (text as NSString).range(of: keyword)
+      )
+    }
+    
+    if let color = color {
+      attributedString.addAttribute(
+        NSAttributedString.Key.foregroundColor,
+        value: color,
+        range: (text as NSString).range(of: keyword)
+      )
+    }
+    
+    if let lineSpace = lineSpace {
+      let style = NSMutableParagraphStyle()
+      style.lineSpacing = lineSpace
+      attributedString.addAttribute(.paragraphStyle,
+                                   value: style,
+                                   range: NSRange(location: 0, length: attributedString.length))
+    }
+    
+    if let lineOffset = lineOffset {
+      attributedString.addAttribute(
+        NSAttributedString.Key.baselineOffset,
+        value: lineOffset,
+        range: (text as NSString).range(of: keyword)
+      )
+    }
+    
+    return attributedString
+  }
+  
+  
+  
+  class func config(
+    text: String,
+    keywords: [String],
+    font: UIFont? = nil,
+    color: UIColor? = nil,
+    lineSpace: CGFloat? = nil,
+    lineOffset: CGFloat? = nil
+  ) -> NSMutableAttributedString {
+    let attributedString = NSMutableAttributedString(string: text)
+    
+    for keyword in keywords {
       if let font = font {
-          attributedString.addAttribute(
-              NSAttributedString.Key.font,
-              value: font,
-              range: (text as NSString).range(of: keyword)
-          )
-      }
-      
-      if let lineOffset = lineOffset {
-          attributedString.addAttribute(
-              NSAttributedString.Key.baselineOffset,
-              value: lineOffset,
-              range: (text as NSString).range(of: keyword)
-          )
+        attributedString.addAttribute(
+          NSAttributedString.Key.font,
+          value: font,
+          range: (text as NSString).range(of: keyword)
+        )
       }
       
       if let color = color {
-          attributedString.addAttribute(
-              NSAttributedString.Key.foregroundColor,
-              value: color,
-              range: (text as NSString).range(of: keyword)
-          )
+        attributedString.addAttribute(
+          NSAttributedString.Key.foregroundColor,
+          value: color,
+          range: (text as NSString).range(of: keyword)
+        )
       }
       
-      return attributedString
+      if let lineSpace = lineSpace {
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = lineSpace
+        attributedString.addAttribute(.paragraphStyle,
+                                     value: style,
+                                     range: NSRange(location: 0, length: attributedString.length))
+      }
+      
+      if let lineOffset = lineOffset {
+        attributedString.addAttribute(
+          NSAttributedString.Key.baselineOffset,
+          value: lineOffset,
+          range: (text as NSString).range(of: keyword)
+        )
+      }
+    }
+    
+    return attributedString
   }
 }
